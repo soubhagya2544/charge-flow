924564d87f7c5c11b9ad6d6a8b0125f69c4599b1
soubhagya2544 <soubhagya2544@gmail.com>
2025-12-07 14:08:23 +0530

    Update from Shipper

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Suppress Chrome extension errors
+window.addEventListener('error', (event) => {
+  if (event.message && event.message.includes('chrome-extension://')) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+});
+
+// Suppress unhandled promise rejections from extensions
+window.addEventListener('unhandledrejection', (event) => {
+  if (event.reason && event.reason.message && event.reason.message.includes('chrome-extension://')) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
